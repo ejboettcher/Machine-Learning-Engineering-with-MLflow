bce194a378c2058f2703516063eb7cdbafd9b4df
Evelyn Boettcher <evelyn.boettcher@gmail.com>
2022-12-09 14:44:42 -0500

    Ran first MLflow pipeline  model

--- Chapter01/run.sh
+++ Chapter01/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker build -t chapter_1_homlflow .
+sudo docker volume create --name chap1
+sudo docker run -p 8888:8888 -p 5000:5000 -v $(pwd):/home/jovyan  -d chapter_1_homlflow
+
 docker run -p 8888:8888 -p 5000:5000 -v $(pwd):/home/jovyan/ -it chapter_1_homlflow
+
+sudo docker run -p 8888:8888 -p 5000:5000 -v `pwd`:/home/jovyan  -u $(id -u):$(id -u) -it chapter_1_homlflow
+
 
--- Chapter01/stockpred/train.py
+++ Chapter01/stockpred/train.py
@@ -19,6 +19,7 @@ def acquire_training_data():
     df = web.DataReader("BTC-USD", 'yahoo', start, end)
     return df
 
+
 def digitize(n):
     if n > 0:
         return 1
@@ -41,44 +42,30 @@ def rolling_window(a, window):
 
 
 def prepare_training_data(data):
-
     """
         Return a prepared numpy dataframe
         input : Dataframe with expected schema
-
     """
     data['Delta'] = data['Close'] - data['Open']
     data['to_predict'] = data['Delta'].apply(lambda d: digitize(d))
     return data
 
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-
     with mlflow.start_run():
-
         training_data = acquire_training_data()
-
         prepared_training_data_df = prepare_training_data(training_data)
-
         btc_mat = prepared_training_data_df.to_numpy()
-
         WINDOW_SIZE = 14
-
         X = rolling_window(btc_mat[:, 7], WINDOW_SIZE)[:-1, :]
         Y = prepared_training_data_df['to_predict'].to_numpy()[WINDOW_SIZE:]
-
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=4284, stratify=Y)
-
         clf = RandomForestClassifier(bootstrap=True, criterion='gini', min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=50, random_state=4284, verbose=0)
-
         clf.fit(X_train, y_train)
-
         predicted = clf.predict(X_test)
-
         mlflow.sklearn.log_model(clf, "model_random_forest")
-
         print(classification_report(y_test, predicted))
-
         mlflow.log_metric("precision_label_0", precision_score(y_test, predicted, pos_label=0))
         mlflow.log_metric("recall_label_0", recall_score(y_test, predicted, pos_label=0))
         mlflow.log_metric("f1score_label_0", f1_score(y_test, predicted, pos_label=0))
